979cd1f9b21436cd52bd96f8e021b2f45e9c79e0
Rahul Waghmare <rahulwaghmare1711@gmail.com>
2025-04-27 19:46:07 +0530

    interview conduction with vapi done

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -6,7 +6,7 @@ import { getRandomInterviewCover } from "@/lib/utils";
 import DisplayTechIcons from "./DisplayTechIcons";
 
 const InterviewCard = ({
-  interviewId,
+  id,
   userId,
   role,
   type,
@@ -57,11 +57,7 @@ const InterviewCard = ({
           <DisplayTechIcons techStack={techstack} />
           <Button className="btn-primary">
             <Link
-              href={
-                feedback
-                  ? `/interview/${interviewId}/feedback`
-                  : `/interview/${interviewId}`
-              }
+              href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}
             >
               {feedback ? "Check FeedBack" : "View Interview"}
             </Link>
